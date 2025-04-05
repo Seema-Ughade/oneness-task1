838e7f4e3bd9d6ee0db1a1aa380a639b922025e4
seema <seemarughade1363@gmail.com>
2025-04-05 15:46:07 +0530

    new currecy complete code

--- src/provider.jsx
+++ src/provider.jsx
@@ -2,16 +2,13 @@
 import { Provider } from "react-redux"
 import { CartProvider } from "./components/contexts/CartContext"
 import { NotificationProvider } from "./components/contexts/NotificationContext"
-import { CurrencyProvider } from "./components/contexts/CurrencyContext"
 import store from "./store"
 
 export default function AppProvider({ children }) {
   return (
     <Provider store={store}>
       <NotificationProvider>   
-        <CurrencyProvider>
           <CartProvider>{children}</CartProvider>
-        </CurrencyProvider>
       </NotificationProvider>
     </Provider>
   )
